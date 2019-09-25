e45ff5240763e69a116621bcc57c0bf8999a9cbf
Shoaib <shoaibmohammed92@gmail.com>
2019-09-25 13:02:22 +0530

    Sep-25

--- src/main/java/com/practise/leetcode/LeetCodeV11.java
+++ src/main/java/com/practise/leetcode/LeetCodeV11.java
@@ -545,5 +545,23 @@ public class LeetCodeV11 {
 		}
 		return max;
 
-	}	
+	}
+
+	public int maxDistance(List<List<Integer>> arrays) {
+
+		int max = arrays.get(0).get(arrays.get(0).size() - 1);
+		int min = arrays.get(0).get(0);
+
+		int result = 0;
+
+		for (int i = 1; i < arrays.size(); i++) {
+
+			result = Math.max(Math.abs(max - arrays.get(i).get(0)),
+					Math.max(result, Math.abs(min - arrays.get(i).get(arrays.get(0).size() - 1))));
+			min = Math.min(min, arrays.get(i).get(0));
+			max = Math.max(max, arrays.get(i).get(arrays.get(0).size() - 1));
+
+		}
+		return result;
+	}
 }
